3b588a97f0d400c0261bcba34d9a48bd4763d7ce
Osama M Safi <mr.osamasafi@gmail.com>
2026-04-12 01:57:05 +0300

    add comment to loop basic 1 task

--- for-loop-basic/app.py
+++ for-loop-basic/app.py
@@ -1,23 +1,28 @@
+# Basic
 for i in range(0,151,1):
     print (i)
 
-
+# Multiples of Five
 for i in range(5,1001,1):
     if i % 5 == 0 :
         print(i)
 
 # or
-
 for i in range(5, 1001, 5):
     print(i)
 
 
+
+# Counting, the Dojo Way
 for i in range(0 ,101 ,1):
     if i % 5 == 0 :
         print('coding')
     else :
         print('coding dojo')
 
+
+
+# Whoa. That Sucker's Huge
 sum = 0
 for i in range(0,500001,1):
     if i % 2 != 0 :
@@ -25,10 +30,14 @@ for i in range(0,500001,1):
 print(sum)
 
 
+
+# Countdown by Fours
 for i in range(2018,-1,-4):
     print(i)
 
 
+
+# Flexible Counter
 lowNum = 2
 highNum = 9
 mult = 3
