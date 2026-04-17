171c91dca91a7588eb20b5c82fd27ee94931be86
Osama M Safi <mr.osamasafi@gmail.com>
2026-04-17 16:43:13 +0300

    build for loop advance

--- for-loop-advance/app.py
+++ for-loop-advance/app.py
@@ -1,63 +1,93 @@
-def a():
-    return 5
-print(a())
+def biggie_size(nums):
+    value = []
+    for i in nums :
+        if(i > 0):
+            value.append('big')
+        else:
+            value.append(i)
+    return value
 
+print(biggie_size([-1, 3, 5, -5]))
 
-def a(): 
-    return 5 
-print(a()+a())
+#######################################
 
+def count_positives(nums):
+    count = 0
+    for i in nums:
+        if i > 0:
+            count += 1
+    nums[-1] = count
+    return nums
+print(count_positives([-1, 1, 1, 1]))
+print(count_positives([1, 6, -4, -2, -7, -2]))
 
-def a(): 
-    return 5 
-    return 10 
-print(a())
+#######################################
 
-def a(): 
-    return 5 
-    print(10) 
-print(a())
+def sum_total(nums):
+    sum = 0
+    for i in nums:
+        sum+=i
+    return sum
+print(sum_total([1,2,3,4]))
 
-def a(): 
-    print(5) 
-x = a() 
-print(x)
+#######################################
 
+def length (nums):
+    count = 0
+    for i in nums:
+        count+=1
+    return count
+print(length([]))
 
-def a(b,c): 
-    print(b+c) 
-print(a(1,2) + a(2,3))
+#######################################
 
+def minimum (nums):
+    val = nums[0]
+    for i in nums:
+        if(i < val):
+            val = i
+    return val
+print (minimum([37,2,1,-9]))
 
-def a(b,c): 
-    return str(b)+str(c) 
-print(a(2,5))
+#######################################
 
-def a(): 
-    b = 100
-    print(b)
-    if b < 10:
-        return 5
-    else:
-        return 10 
-    return 7 
-print(a())
+def maximum (nums):
+    val = nums[0]
+    for i in nums:
+        if(i > val):
+            val = i
+    return val
+print (maximum([37,2,1,-9]))
 
+#######################################
 
-def a(b,c): 
-    if b<c: 
-        return 7 
-    else: 
-        return 14 
-    return 3 
-print(a(2,3)) 
-print(a(5,3)) 
-print(a(2,3) + a(5,3))
+def ultimate_analysis(nums):
+    min = nums[0]
+    max = nums[0]
+    count = 0
+    sum = 0
+    for i in nums:
+        count+=1
+        sum+=i
+        if(i < min):
+            min = i
+        else:
+            max = i
+    avarage = sum / count
+    return {
+        'sumTotal' : sum,
+        'avarage' : avarage,
+        'minimum' : min,
+        'maximum' : max,
+        'lenght' : count
+    }
 
+print(ultimate_analysis([37,2,1,-9]))
 
-def a(b,c): 
-    return b+c 
-    return 10 
-print(a(3,5))
+#######################################
 
-
+def reverse_list(nums):
+    for i in range(len(nums) // 2):
+        nums[i], nums[len(nums) - 1 - i] = nums[len(nums) - 1 - i], nums[i]
+    return nums
+print(reverse_list([37, 2, 1, -9]))
