2617eea1fd7146e11adab650e3354cee1d15ae02
Osama M Safi <mr.osamasafi@gmail.com>
2026-04-18 15:40:37 +0300

    build function intermediate advance task

---
+++ Functions_Intermediate_Advance/app.py
@@ -0,0 +1,63 @@
+x = [ [5,2,3], [10,8,9] ]
+students = [
+    {'first_name':  'Michael', 'last_name' : 'Jordan'},
+    {'first_name' : 'John', 'last_name' : 'Rosales'}
+]
+sports_directory = {
+    'basketball' : ['Kobe', 'Jordan', 'James', 'Curry'],
+    'soccer' : ['Messi', 'Ronaldo', 'Rooney']
+}
+z = [ {'x': 10, 'y': 20} ]
+
+x[1][0] = 15
+students[0]['last_name'] = 'Bryant'
+sports_directory['soccer'][0] = 'Andres'
+z[0]['y'] = 30
+
+################################################
+
+def iterateDictionary(some_list):
+    for dictionary in some_list:
+        output = ""
+        for key, value in dictionary.items():
+            output += f"{key} - {value}, "
+        
+        print(output[:-2])
+
+students = [
+    {'first_name':  'Michael', 'last_name' : 'Jordan'},
+    {'first_name' : 'John', 'last_name' : 'Rosales'},
+    {'first_name' : 'Mark', 'last_name' : 'Guillen'},
+    {'first_name' : 'KB', 'last_name' : 'Tonel'}
+]
+
+iterateDictionary(students)
+
+################################################
+
+def iterateDictionary2(key_name, list):
+    for i in list:
+        print(i[key_name])
+
+students = [
+    {'first_name': 'Michael', 'last_name' : 'Jordan'},
+    {'first_name' : 'John', 'last_name' : 'Rosales'},
+    {'first_name' : 'Mark', 'last_name' : 'Guillen'},
+    {'first_name' : 'KB', 'last_name' : 'Tonel'}
+]
+iterateDictionary2('first_name', students)
+
+################################################
+
+def printInfo(some_dict):
+    for key in some_dict:
+        length_list = len(some_dict[key])
+        print(f"{length_list} {key.upper()}")
+        for item in some_dict[key]:
+            print(item)
+        print('')
+dojo = {
+    'locations': ['San Jose', 'Seattle', 'Dallas', 'Chicago', 'Tulsa', 'DC', 'Burbank'],
+    'instructors': ['Michael', 'Amy', 'Eduardo', 'Josh', 'Graham', 'Patrick', 'Minh', 'Devon']
+}
+printInfo(dojo)
