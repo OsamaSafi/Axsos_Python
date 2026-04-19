dff73a6651b55a329cd991c7108168f7a4e0855a
Osama M Safi <mr.osamasafi@gmail.com>
2026-04-19 14:24:39 +0300

    build insertion sort task

---
+++ insertion_sort/app.py
@@ -0,0 +1,9 @@
+def insertion_sort(nums):
+    for i in range(1, len(nums)):
+        key = nums[i] 
+        j = i - 1
+        while j >= 0 and key < nums[j]:
+            nums[j + 1] = nums[j]
+            j = j - 1
+        nums[j + 1] = key
+    return nums
