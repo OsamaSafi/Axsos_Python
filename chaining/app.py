8cc904c33144980b8cf0916266a64bf130fba225
Osama M Safi <mr.osamasafi@gmail.com>
2026-04-22 00:26:21 +0300

    build chaining task

--- chaining/app.py
+++ chaining/app.py
@@ -13,6 +13,7 @@ class User:
     
     def display_user_balance(self):
         print(f"User: {self.name} , Balance ${self.balance}")
+        return self
 
     def transfer_money(self,other_user,amount):
         self.make_withdrow(amount)
@@ -23,26 +24,10 @@ osama = User('osama')
 omar = User('omar')
 nesma = User('nesma')
 
-osama.make_deposit(50)
-osama.make_deposit(100)
-osama.make_deposit(150)
-osama.make_withdrow(130)
-osama.display_user_balance()
-
-omar.make_deposit(100)
-omar.make_deposit(200)
-omar.make_withdrow(50)
-omar.make_withdrow(70)
-omar.display_user_balance()
-
-nesma.make_deposit(500)
-nesma.make_withdrow(30)
-nesma.make_withdrow(100)
-nesma.make_withdrow(140)
-nesma.display_user_balance()
+osama.make_deposit(50).make_deposit(100).make_deposit(150).make_withdrow(130).display_user_balance()
+omar.make_deposit(100).make_deposit(200).make_withdrow(50).make_withdrow(70).display_user_balance()
+nesma.make_deposit(500).make_withdrow(30).make_withdrow(100).make_withdrow(140).display_user_balance()
 print("#######################")
-
-osama.transfer_money(nesma,100)
-osama.display_user_balance()
+osama.transfer_money(nesma,100).display_user_balance()
 nesma.display_user_balance()
 
